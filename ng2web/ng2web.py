20c735bcfdbb3badf5fb7dfee96bc7dda714273e
Dave Pearson <davep@davep.org>
2021-11-07 13:22:49 +0000

    :sparkles: Mapping out the structure of the tool.

---
+++ ng2web/ng2web.py
@@ -0,0 +1,57 @@
+"""Core code."""
+
+##############################################################################
+# Python imports.
+import argparse
+
+##############################################################################
+# Third party imports.
+from ngdb import __version__ as ngdb_ver
+
+##############################################################################
+# Local imports.
+from . import __version__
+
+##############################################################################
+# Parse the arguments.
+def get_args() -> argparse.Namespace:
+    """Get the arguments passed by the user.
+
+    :returns: The parsed arguments.
+    :rtype: ~argparse.Namespace
+    """
+
+    # Version information, used in a couple of paces.
+    version = f"v{__version__} (ngdb v{ngdb_ver})"
+
+    # Create the argument parser object.
+    parser = argparse.ArgumentParser(
+        description = "Convert a Norton Guide database to HTML documents.",
+        epilog      = version
+    )
+
+    # Add --version
+    parser.add_argument(
+        "-v", "--version",
+        help    = "Show version information.",
+        action  = "version",
+        version = f"%(prog)s {version}"
+    )
+
+    # The remainder is the path to the guides to look at.
+    parser.add_argument( "guide", help="The guide to convert" )
+
+    # Parse the command line.
+    return parser.parse_args()
+
+##############################################################################
+# Main entry point for the tool.
+def main() -> None:
+    """Main entry point for the tool."""
+
+    # Get the arguments.
+    args = get_args()
+
+    print( f"I'd convert {args.guide} here" )
+
+### ng2web.py ends here
