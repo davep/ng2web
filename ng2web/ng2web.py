bcdb3805f9167392a01506a75e622c9475cd77c1
Dave Pearson <davep@davep.org>
2024-03-31 14:17:11 +0100

    :art: Reformat the code with black

--- ng2web/ng2web.py
+++ ng2web/ng2web.py
@@ -3,17 +3,17 @@
 ##############################################################################
 # Python imports.
 from __future__ import annotations
-from pathlib    import Path
+from pathlib import Path
 import argparse
 
 ##############################################################################
 # Third party imports.
-from ngdb import (
-    __version__ as ngdb_ver, NortonGuide, Entry, MarkupText, make_dos_like
-)
+from ngdb import __version__ as ngdb_ver, NortonGuide, Entry, MarkupText, make_dos_like
 from jinja2 import (
     __version__ as jinja_version,
-    Environment, PackageLoader, select_autoescape
+    Environment,
+    PackageLoader,
+    select_autoescape,
 )
 from markupsafe import Markup, escape
 
@@ -21,8 +21,9 @@ from markupsafe import Markup, escape
 # Local imports.
 from . import __version__
 
+
 ##############################################################################
-def log( msg: str ) -> None:
+def log(msg: str) -> None:
     """Simple logging function.
 
     Args:
@@ -31,10 +32,11 @@ def log( msg: str ) -> None:
     At some point soon I'll possibly switch to proper logging, but just for
     now...
     """
-    print( msg )
+    print(msg)
+
 
 ##############################################################################
-def prefix( text: str, guide: NortonGuide ) -> str:
+def prefix(text: str, guide: NortonGuide) -> str:
     """Prefix the given text with the guide's namespace.
 
     Args:
@@ -46,8 +48,9 @@ def prefix( text: str, guide: NortonGuide ) -> str:
     """
     return f"{guide.path.stem}-{text}"
 
+
 ##############################################################################
-def output( args: argparse.Namespace, file_name: Path | str ) -> Path:
+def output(args: argparse.Namespace, file_name: Path | str) -> Path:
     """Expand a file's name so that it's within the output location.
 
     Args:
@@ -61,7 +64,8 @@ def output( args: argparse.Namespace, file_name: Path | str ) -> Path:
        This function will expand any user information within the specified
        output path and also resolve the result.
     """
-    return Path( args.output ).expanduser().resolve() / Path( file_name )
+    return Path(args.output).expanduser().resolve() / Path(file_name)
+
 
 ##############################################################################
 def get_args() -> argparse.Namespace:
@@ -76,34 +80,37 @@ def get_args() -> argparse.Namespace:
 
     # Create the argument parser object.
     parser = argparse.ArgumentParser(
-        prog        = Path( __file__ ).stem,
-        description = "Convert a Norton Guide database to HTML documents.",
-        epilog      = version
+        prog=Path(__file__).stem,
+        description="Convert a Norton Guide database to HTML documents.",
+        epilog=version,
     )
 
     # Add an optional output directory.
     parser.add_argument(
-        "-o", "--output",
-        help    = "Directory where the output files will be created.",
-        default = "."
+        "-o",
+        "--output",
+        help="Directory where the output files will be created.",
+        default=".",
     )
 
     # Add --version
     parser.add_argument(
-        "-v", "--version",
-        help    = "Show version information.",
-        action  = "version",
-        version = f"%(prog)s {version}"
+        "-v",
+        "--version",
+        help="Show version information.",
+        action="version",
+        version=f"%(prog)s {version}",
     )
 
     # The remainder is the path to the guides to look at.
-    parser.add_argument( "guide", help="The guide to convert" )
+    parser.add_argument("guide", help="The guide to convert")
 
     # Parse the command line.
     return parser.parse_args()
 
+
 ##############################################################################
-def about( guide: NortonGuide, args: argparse.Namespace ) -> Path:
+def about(guide: NortonGuide, args: argparse.Namespace) -> Path:
     """Get the name of the about page for the guide.
 
     Args:
@@ -113,10 +120,11 @@ def about( guide: NortonGuide, args: argparse.Namespace ) -> Path:
     Returns:
         Path: The path to the about file for the guide.
     """
-    return output( args, prefix( "about.html", guide ) )
+    return output(args, prefix("about.html", guide))
+
 
 ##############################################################################
-def write_about( guide: NortonGuide, args: argparse.Namespace, env: Environment ) -> None:
+def write_about(guide: NortonGuide, args: argparse.Namespace, env: Environment) -> None:
     """Write the about page for the guide.
 
     Args:
@@ -124,12 +132,13 @@ def write_about( guide: NortonGuide, args: argparse.Namespace, env: Environment 
         args (argparse.Namespace): The command line arguments.
         env (Environment): The template environment.
     """
-    log( f"Writing about into {about( guide, args )}" )
-    with about( guide, args ).open( "w" ) as target:
-        target.write( env.get_template( "about.html" ).render() )
+    log(f"Writing about into {about( guide, args )}")
+    with about(guide, args).open("w") as target:
+        target.write(env.get_template("about.html").render())
+
 
 ##############################################################################
-def css( guide: NortonGuide, args: argparse.Namespace ) -> Path:
+def css(guide: NortonGuide, args: argparse.Namespace) -> Path:
     """Get the name of the stylesheet for the guide.
 
     Args:
@@ -139,10 +148,11 @@ def css( guide: NortonGuide, args: argparse.Namespace ) -> Path:
     Returns:
         Path: The path to the stylesheet for the guide.
     """
-    return output( args, prefix( "style.css", guide ) )
+    return output(args, prefix("style.css", guide))
+
 
 ##############################################################################
-def write_css( guide: NortonGuide, args: argparse.Namespace, env: Environment ) -> None:
+def write_css(guide: NortonGuide, args: argparse.Namespace, env: Environment) -> None:
     """Write the stylesheet for the guide.
 
     Args:
@@ -150,32 +160,38 @@ def write_css( guide: NortonGuide, args: argparse.Namespace, env: Environment ) 
         args (argparse.Namespace): The command line arguments.
         env (Environment): The template environment.
     """
-    log( f"Writing stylesheet into {css( guide, args )}" )
-    with css( guide, args ).open( "w" ) as target:
-        target.write( env.get_template( "base.css" ).render(
-            colours = enumerate( (
-                "black",
-                "navy",
-                "green",
-                "teal",
-                "maroon",
-                "purple",
-                "olive",
-                "silver",
-                "gray",
-                "blue",
-                "lime",
-                "aqua",
-                "red",
-                "fuchsia",
-                "yellow",
-                "white"
-            ) )
-        ) )
+    log(f"Writing stylesheet into {css( guide, args )}")
+    with css(guide, args).open("w") as target:
+        target.write(
+            env.get_template("base.css").render(
+                colours=enumerate(
+                    (
+                        "black",
+                        "navy",
+                        "green",
+                        "teal",
+                        "maroon",
+                        "purple",
+                        "olive",
+                        "silver",
+                        "gray",
+                        "blue",
+                        "lime",
+                        "aqua",
+                        "red",
+                        "fuchsia",
+                        "yellow",
+                        "white",
+                    )
+                )
+            )
+        )
+
 
 ##############################################################################
-def entry_file( guide: NortonGuide,
-           args: argparse.Namespace, location: int | Entry ) -> Path:
+def entry_file(
+    guide: NortonGuide, args: argparse.Namespace, location: int | Entry
+) -> Path:
     """Get the name of an entry in the guide.
 
     Args:
@@ -188,12 +204,17 @@ def entry_file( guide: NortonGuide,
     """
     return output(
         args,
-        prefix( f"{ location if isinstance( location, int ) else location.offset }.html", guide )
+        prefix(
+            f"{ location if isinstance( location, int ) else location.offset }.html",
+            guide,
+        ),
     )
 
+
 ##############################################################################
-def write_entry( entry: Entry,
-                 guide: NortonGuide, args: argparse.Namespace, env: Environment ) -> None:
+def write_entry(
+    entry: Entry, guide: NortonGuide, args: argparse.Namespace, env: Environment
+) -> None:
     """Write the an entry from the guide.
 
     Args:
@@ -202,28 +223,35 @@ def write_entry( entry: Entry,
         args (argparse.Namespace): The command line arguments.
         env (Environment): The template environment.
     """
-    log( f"Writing {entry.__class__.__name__.lower()} entry to {entry_file( guide, args, entry )}" )
-    with entry_file( guide, args, entry ).open( "w" ) as target:
+    log(
+        f"Writing {entry.__class__.__name__.lower()} entry to {entry_file( guide, args, entry )}"
+    )
+    with entry_file(guide, args, entry).open("w") as target:
         target.write(
-            env.get_template( f"{entry.__class__.__name__.lower()}.html" ).render(
-                entry        = entry,
-                previous_url = entry_file(
-                    guide, args, entry.previous
-                ).name if entry.has_previous else None,
-                next_url     = entry_file(
-                    guide, args, entry.next
-                ) if entry.has_next else None,
-                up_url       = entry_file(
-                    guide, args, entry.parent.offset
-                ).name if entry.parent else None
+            env.get_template(f"{entry.__class__.__name__.lower()}.html").render(
+                entry=entry,
+                previous_url=(
+                    entry_file(guide, args, entry.previous).name
+                    if entry.has_previous
+                    else None
+                ),
+                next_url=(
+                    entry_file(guide, args, entry.next) if entry.has_next else None
+                ),
+                up_url=(
+                    entry_file(guide, args, entry.parent.offset).name
+                    if entry.parent
+                    else None
+                ),
             )
         )
 
+
 ##############################################################################
-class ToHTML( MarkupText ):
+class ToHTML(MarkupText):
     """Class to convert some Norton Guide source into HTML"""
 
-    def open_markup( self, cls: str ) -> str:
+    def open_markup(self, cls: str) -> str:
         """Open markup for the given class.
 
         Args:
@@ -234,7 +262,7 @@ class ToHTML( MarkupText ):
         """
         return f'<span class="{cls}">'
 
-    def close_markup( self, cls: str ) -> str:
+    def close_markup(self, cls: str) -> str:
         """Close markup for the given class.
 
         Args:
@@ -246,48 +274,49 @@ class ToHTML( MarkupText ):
         del cls
         return "</span>"
 
-    def text( self, text: str ) -> None:
+    def text(self, text: str) -> None:
         """Handle the given text.
 
         Args:
             text (str): The text to handle.
         """
-        super().text( str( escape( make_dos_like( text ) ) ) )
+        super().text(str(escape(make_dos_like(text))))
 
-    def colour( self, colour: int ) -> None:
+    def colour(self, colour: int) -> None:
         """Handle the given colour value.
 
         Args:
             colour (int): The colour value to handle.
         """
-        self.begin_markup( f"fg{ colour & 0xF} bg{ colour >> 4}" )
+        self.begin_markup(f"fg{ colour & 0xF} bg{ colour >> 4}")
 
-    def bold( self ) -> None:
+    def bold(self) -> None:
         """Handle being asked to go to bold mode."""
-        self.begin_markup( "ngb" )
+        self.begin_markup("ngb")
 
-    def unbold( self ) -> None:
+    def unbold(self) -> None:
         """Handle being asked to go out of bold mode."""
         self.end_markup()
 
-    def reverse( self ) -> None:
+    def reverse(self) -> None:
         """Handle being asked to go to reverse mode."""
-        self.begin_markup( "ngr" )
+        self.begin_markup("ngr")
 
-    def unreverse( self ) -> None:
+    def unreverse(self) -> None:
         """Handle being asked to go out of reverse mode."""
         self.end_markup()
 
-    def underline( self ) -> None:
+    def underline(self) -> None:
         """Handle being asked to go in underline mode."""
-        self.begin_markup( "ngu" )
+        self.begin_markup("ngu")
 
-    def ununderline( self ) -> None:
+    def ununderline(self) -> None:
         """Handle being asked to go out of underline mode."""
         self.end_markup()
 
+
 ##############################################################################
-def page_title( guide: NortonGuide, entry: Entry | None = None ) -> str:
+def page_title(guide: NortonGuide, entry: Entry | None = None) -> str:
     """Generate a title appropriate for the current page.
 
     Args:
@@ -299,21 +328,22 @@ def page_title( guide: NortonGuide, entry: Entry | None = None ) -> str:
     """
 
     # Start with the guide title.
-    title = [ guide.title ]
+    title = [guide.title]
 
     # If there's a parent menu...
     if entry and entry.parent.has_menu:
-        title += [ guide.menus[ entry.parent.menu ].title ]
+        title += [guide.menus[entry.parent.menu].title]
 
     # If there's a parent menu prompt...
     if entry and entry.parent.has_prompt:
-        title += [ guide.menus[ entry.parent.menu ].prompts[ entry.parent.prompt ] ]
+        title += [guide.menus[entry.parent.menu].prompts[entry.parent.prompt]]
 
     # Join it all up.
-    return " » ".join( make_dos_like( part ) for part in title )
+    return " » ".join(make_dos_like(part) for part in title)
+
 
 ##############################################################################
-def to_html( args: argparse.Namespace ) -> None:
+def to_html(args: argparse.Namespace) -> None:
     """Convert a Norton Guide into HTML.
 
     Args:
@@ -322,47 +352,48 @@ def to_html( args: argparse.Namespace ) -> None:
 
     # Open the guide. Note that we turn it into a Path, and just to be kind
     # to folk, we attempt to expand any sort of ~ inside it first.
-    with NortonGuide( Path( args.guide ).expanduser().resolve() ) as guide:
+    with NortonGuide(Path(args.guide).expanduser().resolve()) as guide:
 
         # Log some basics.
-        log( f"Guide: {guide.path}" )
-        log( f"Output prefix: {prefix( '', guide )}" )
+        log(f"Guide: {guide.path}")
+        log(f"Output prefix: {prefix( '', guide )}")
 
         # Bootstrap the template stuff.
         env = Environment(
-            loader     = PackageLoader( Path( __file__ ).stem ),
-            autoescape = select_autoescape()
+            loader=PackageLoader(Path(__file__).stem), autoescape=select_autoescape()
         )
 
         # Set up the global variables for template expansion.
         env.globals = dict(
-            generator  = f"ng2web v{__version__} (ngdb v{ngdb_ver})",
-            guide      = guide,
-            about_url  = about( guide, args ).name,
-            stylesheet = css( guide, args ).name
+            generator=f"ng2web v{__version__} (ngdb v{ngdb_ver})",
+            guide=guide,
+            about_url=about(guide, args).name,
+            stylesheet=css(guide, args).name,
         )
 
         # Set up the filters for the guide templates.
         env.filters = dict(
-            urlify = lambda option: entry_file( guide, args, option.offset ).name,
-            toHTML = lambda src: Markup( ToHTML( src ) ),
-            title  = lambda entry: page_title( guide, entry )
+            urlify=lambda option: entry_file(guide, args, option.offset).name,
+            toHTML=lambda src: Markup(ToHTML(src)),
+            title=lambda entry: page_title(guide, entry),
         )
 
         # Write the stylesheet.
-        write_css( guide, args, env )
+        write_css(guide, args, env)
 
         # Write the about page.
-        write_about( guide, args, env )
+        write_about(guide, args, env)
 
         # Now, for every entry in the guide...
         for entry in guide:
-            write_entry( entry, guide, args, env )
+            write_entry(entry, guide, args, env)
+
 
 ##############################################################################
 # Main entry point for the tool.
 def main() -> None:
     """Main entry point for the tool."""
-    to_html( get_args() )
+    to_html(get_args())
+
 
 ### ng2web.py ends here
